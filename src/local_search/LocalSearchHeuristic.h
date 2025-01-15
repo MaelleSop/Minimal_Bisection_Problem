357d5f85523499d1e2cb965d7cc7933b130d88a9
MaelleSop <108056541+MaelleSop@users.noreply.github.com>
2025-01-15 15:03:58 -0500

    Update

---
+++ src/local_search/LocalSearchHeuristic.h
@@ -0,0 +1,10 @@
+#ifndef GRAPHTHEORYPROJECT_GROUP2_LOCALSEARCHHEURISTIC_H
+#define GRAPHTHEORYPROJECT_GROUP2_LOCALSEARCHHEURISTIC_H
+
+#include "../model/GraphAdjacencyList.h"
+#include "../constructive/ConstructiveHeuristic.h"
+
+// #define TEST
+vector<vector<int>> LocalSearch(GraphAdjacencyList &graph, vector<vector<int>> subgraphs);
+
+#endif //GRAPHTHEORYPROJECT_GROUP2_LOCALSEARCHHEURISTIC_H
