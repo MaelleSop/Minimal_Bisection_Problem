5a95ce79d1b24c5a15c0b61091f850a4d9694e0c
MaelleSop <108056541+MaelleSop@users.noreply.github.com>
2025-01-15 15:05:57 -0500

    Update

---
+++ src/tabu_search/TabuSearch.h
@@ -0,0 +1,11 @@
+#ifndef GRAPHTHEORYPROJECT_GROUP2_TABUSEARCH_H
+#define GRAPHTHEORYPROJECT_GROUP2_TABUSEARCH_H
+
+#include "../model/GraphAdjacencyList.h"
+#include "../constructive/ConstructiveHeuristic.h"
+#include "../local_search/LocalSearchHeuristic.h"
+
+vector<vector<int>> TabuSearch(GraphAdjacencyList &graph);
+
+
+#endif //GRAPHTHEORYPROJECT_GROUP2_TABUSEARCH_H
